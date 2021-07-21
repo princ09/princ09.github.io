f729400680f1493566318227213334873a453164
raazkumar <rajkumar@flairlabs.com>
2021-07-21 14:29:05 +0530

    Anime Friends react hook app added

---
+++ pages/anime_friends_react_hooks_app/precache-manifest.7f3f9692320270ddec95e68be4cdfe2a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e38a94db7804865f7a9aaee4661333c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92d30aea58a2ef389604",
+    "url": "/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "bade5c7b706d88fdc8d5",
+    "url": "/static/css/main.4d6722d4.chunk.css"
+  },
+  {
+    "revision": "92d30aea58a2ef389604",
+    "url": "/static/js/2.765c0106.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.765c0106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bade5c7b706d88fdc8d5",
+    "url": "/static/js/main.36f38f2c.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
